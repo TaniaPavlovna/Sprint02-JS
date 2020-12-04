70c6ae63fa691616f8dae8e598138a3cb90297b5
Tania <tetiana.pitikova@gmail.com>
2020-12-04 22:32:03 +0200

    t02

---
+++ t02/js/script.js
@@ -0,0 +1,12 @@
+let quote;
+alert(quote);
+quote = "With great power, comes great responsibility";
+alert(quote);
+displayAuthor();
+alert(quote);
+
+function displayAuthor() {
+	let author = 'Spiderman';
+	alert(author);
+	quote = '...';
+}
