b74ff39db0ec3b4eb3f5c65714d490f1677a13ce
Tania <tetiana.pitikova@gmail.com>
2020-12-04 22:44:45 +0200

    add t06

---
+++ t06/js/script.js
@@ -0,0 +1,21 @@
+var n = 5;
+var time = 90071992547409912n;
+var str = "Hello";
+var isFalse = false;
+var age = null;
+var data = undefined;
+var persone = {name : 'Tania'}
+var id = Symbol("id");
+var func = function myFunc(){};
+
+
+var result = `n is ${typeof n}\ntime is ${typeof time}\nstr is ${typeof str}\nisFalse is ${typeof isFalse}\nage is ${typeof age}\ndata is ${typeof data}\npersone is ${typeof persone}\nid is ${typeof id}\nfunc is ${typeof func}\n`;
+
+alert(result);
+
+
+
+
+
+
+
