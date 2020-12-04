e7dec924529c7402a6170a3733c929bb72554124
Tania <tetiana.pitikova@gmail.com>
2020-12-04 22:34:58 +0200

    add t03

---
+++ t03/js/script.js
@@ -0,0 +1,10 @@
+function myFunc() {
+	alert('Freedom is power')
+};
+
+function countMySquare(number) {
+	alert('The squar of ' + number + ' = ' + (number * number));
+}
+
+myFunc();
+countMySquare(5);
